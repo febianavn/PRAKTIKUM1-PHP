099622934c2dc04f4db6f83ff76a92290d4bfe1d
febianavn <129649692+febianavn@users.noreply.github.com>
2023-04-02 15:05:30 +0100

    Add files via upload

---
+++ Lat4_3b.php
@@ -0,0 +1,8 @@
+<?php
+require_once("Lat4_3a.php");
+
+$mhsl=new mahasiswa();
+$mhsl->nama="hendra";
+echo $mhsl->nama;
+
+?>
---
+++ Lat4_4b.php
@@ -0,0 +1,7 @@
+<?php
+require_once("Lat4_4a.php");
+
+$as = new asisten ();
+$as->setNama("tes");
+echo $as->getNama();
+?>
